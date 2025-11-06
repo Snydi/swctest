b01e4ee53e741f88d868e8ef95cd4b6f18a9574c
snydi <d-snyatkov@mail.ru>
2025-11-06 21:44:37 +0300

    fix: добавлены недостающие файлы для работы авторизации

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required', Password::min(8)->letters()->mixedCase()->numbers()]
+        ];
+    }
+}
